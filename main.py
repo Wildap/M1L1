410970c439fc800d5df73d8e3018f6c0b2810148
Wildap <156494703+Wildap@users.noreply.github.com>
2024-02-03 12:24:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,39 @@
-import time
-meme_dict = {
-            "CRINGE": "Sesuatu yang sangat aneh atau memalukan",
-            "LOL": "Tanggapan umum terhadap sesuatu yang lucu",
-            "ROFL" : "Tanggapan terhadap lelucon",
-            "SHEESH" : "Sedikit ketidaksetujuan",
-            "CREEPY" : "Menakutkan, tidak menyenangkan",
-            "AGGRO" : "Untuk menjadi agresif/marah",
-            }
-for i in range(5):
-    word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ")
-    if word in meme_dict.keys():
-        # Apa yang harus kita lakukan jika kata itu ditemukan?
-        arti = meme_dict[word]
-        print(arti)
-        time.sleep(1)
-    else:
-        # Apa yang harus kita lakukan jika kata itu tidak ditemukan?
-        print("TIDAK DITEMUKAN")
-        time.sleep(1)
+import discord
+import random
+import asyncio
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        print('------')
+
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswith('$guess'):
+            await message.channel.send('Guess a number between 1 and 10.')
+
+            def is_correct(m):
+                return m.author == message.author and m.content.isdigit()
+
+            answer = random.randint(1, 10)
+
+            try:
+                guess = await self.wait_for('message', check=is_correct, timeout=5.0)
+            except asyncio.TimeoutError:
+                return await message.channel.send(f'Sorry, you took too long it was {answer}.')
+
+            if int(guess.content) == answer:
+                await message.channel.send('You are right!')
+            else:
+                await message.channel.send(f'Oops. It is actually {answer}.')
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run('token')
